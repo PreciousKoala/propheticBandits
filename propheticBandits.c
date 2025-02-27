1be0613d9a194e9f18678329d28316501c1adf28
PreciousKoala <koalaprecious@gmail.com>
2025-02-27 19:05:00 +0200

    Now reads file

--- propheticBandits.c
+++ propheticBandits.c
@@ -1,24 +1,25 @@
 #include <getopt.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
-  int maxItems = 1;
-  int keepItemsFlag = 0;
-  int epsilonGreedyFlag = 0;
-  int usb1Flag = 0;
-  int exp3Flag = 0;
+  uint32_t maxItems = 1;
+  uint8_t keepItemsFlag = 0;
+  uint8_t epsilonGreedyFlag = 0;
+  uint8_t usb1Flag = 0;
+  uint8_t exp3Flag = 0;
 
   int opt;
 
   opterr = 0;
 
-  while ((opt = getopt(argc, argv, ":hm:keux")) != -1) {
+  while ((opt = getopt(argc, argv, ":h:m:keux")) != -1) {
     switch (opt) {
     case 'h':
       printf("Usage:\n");
       printf("    propheticBandits [-keux] [-m <integer>] <file>\n");
-      printf("    propheticBandits -h      #Display this help screen.\n\n");
+      printf("    propheticBandits -h      # Display this help screen.\n\n");
       printf("Options:\n");
       printf("    -m <integer>    Set the number of maximum held items.\n");
       printf("    -k              Set the program to be able to keep items at "
@@ -28,7 +29,8 @@ int main(int argc, char **argv) {
       printf("    -x              Run the EXP3 algorithm.\n");
       return 0;
     case 'm':
-      maxItems = atoi(optarg);
+      if (optind < argc)
+        maxItems = atoi(optarg);
       break;
     case 'k':
       keepItemsFlag = 1;
@@ -51,21 +53,32 @@ int main(int argc, char **argv) {
     }
   }
 
-  // Opens data file
+  double *data;
+  uint64_t totalRounds, pricesPerRound;
+  // opens binary data file
   if (optind < argc) {
-    // First non-option argument
+    // small hack to get first non-option argument because getopt is a pain
     char *filename = argv[optind];
-    FILE *file = fopen(filename, "r");
+    FILE *file = fopen(filename, "rb");
     if (!file) {
       printf("Error opening file\n");
       return 1;
     }
-    printf("Processing file: %s\n", filename);
+
+    // first 2 values are 64bit integers
+    fread(&totalRounds, sizeof(uint64_t), 1, file);
+    fread(&pricesPerRound, sizeof(uint64_t), 1, file);
+
+    // allocate memory and get the actual data
+    data = malloc(totalRounds * pricesPerRound * sizeof(double));
+    fread(data, sizeof(double), totalRounds * pricesPerRound, file);
+
     fclose(file);
   } else {
     printf("Error: No filename provided\n");
     return 1;
   }
 
+  free(data);
   return 0;
 }
